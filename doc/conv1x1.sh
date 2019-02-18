6c187deac10958dba3ceddba3856906bd08c57af
feiw <Fei.Wang@amd.com>
2019-02-18 10:11:51 +0800

    add docs

---
+++ doc/conv1x1.sh
@@ -0,0 +1,64 @@
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 28 -W 28 -c 192 -k 64 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 14 -W 14 -c 512 -k 192 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 7 -W 7 -c 832 -k 256 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 14 -W 14 -c 256 -k 256 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 7 -W 7 -c 512 -k 512 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 14 -W 14 -c 256 -k 256 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 7 -W 7 -c 512 -k 512 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 112 -W 112 -c 64 -k 64 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 56 -W 56 -c 64 -k 256 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 56 -W 56 -c 256 -k 64 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 28 -W 28 -c 128 -k 512 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 28 -W 28 -c 512 -k 128 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 14 -W 14 -c 256 -k 1024 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 14 -W 14 -c 1024 -k 256 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 14 -W 14 -c 256 -k 1024 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 7 -W 7 -c 512 -k 2048 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 7 -W 7 -c 2048 -k 512 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 112 -W 112 -c 64 -k 64 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 56 -W 56 -c 64 -k 256 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 56 -W 56 -c 256 -k 64 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 28 -W 28 -c 128 -k 512 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 28 -W 28 -c 512 -k 128 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 14 -W 14 -c 256 -k 1024 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 14 -W 14 -c 1024 -k 256 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 14 -W 14 -c 256 -k 1024 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 7 -W 7 -c 512 -k 2048 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 7 -W 7 -c 2048 -k 512 -x 1 -y 1 -t 1
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 56 -W 56 -c 64 -k 256 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 28 -W 28 -c 128 -k 512 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 14 -W 14 -c 256 -k 1024 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 7 -W 7 -c 2048 -k 512 -x 1 -y 1 -t 1 -u 2 -v 2 -p 3 -q 3
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 56 -W 56 -c 64 -k 256 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 28 -W 28 -c 128 -k 512 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 14 -W 14 -c 256 -k 1024 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 56 -W 56 -c 256 -k 128 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 28 -W 28 -c 512 -k 256 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 28 -W 28 -c 512 -k 1024 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 14 -W 14 -c 1024 -k 512 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 8 -H 14 -W 14 -c 1024 -k 2048 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 56 -W 56 -c 256 -k 128 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 28 -W 28 -c 512 -k 256 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 28 -W 28 -c 512 -k 1024 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 14 -W 14 -c 1024 -k 512 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+./bin/MIOpenDriver conv -i 1 -V 0 -F 0 -s 0 -n 16 -H 14 -W 14 -c 1024 -k 2048 -x 1 -y 1 -t 1 -u 2 -v 2 -p 0 -q 0
+
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 7 -W 7 -c 512 -k 2048 -n 1
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 7 -W 7 -c 512 -k 2048 -n 4
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 7 -W 7 -c 512 -k 2048 -n 8
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 7 -W 7 -c 512 -k 2048 -n 16
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 7 -W 7 -c 512 -k 2048 -n 32
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 7 -W 7 -c 1024 -k 512 -n 1
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 7 -W 7 -c 1024 -k 512 -n 4
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 7 -W 7 -c 1024 -k 512 -n 8
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 7 -W 7 -c 1024 -k 512 -n 16
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 7 -W 7 -c 1024 -k 512 -n 32
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 7 -W 7 -c 2048 -k 512 -n 1
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 7 -W 7 -c 2048 -k 512 -n 4
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 7 -W 7 -c 2048 -k 512 -n 8
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 7 -W 7 -c 2048 -k 512 -n 16
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 7 -W 7 -c 2048 -k 512 -n 
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 14 -W 14 -c 256 -k 1024 -n 32
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 14 -W 14 -c 512 -k 512 -n 32
+./MIOpenDriver conv -i 10 -V 0 -F 1 -s 1 -x 1 -y 1 -t 1 -n 1 -H 14 -W 14 -c 1024 -k 256 -n 32
+
